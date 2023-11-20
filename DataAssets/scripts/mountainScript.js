28a81b98f6a48b27ae93f69e3807e764bcf73dba
Sebastian Acosta <mu9seb@gmail.com>
2023-11-20 10:54:40 -0700

    Mountain display card programmed.

--- DataAssets/scripts/mountainScript.js
+++ DataAssets/scripts/mountainScript.js
@@ -1,6 +1,10 @@
-window.onload = () => {
+document.addEventListener("DOMContentLoaded", () => {
     initMountainsDropdown();
-}
+    
+    const mountainSelectMenu = document.getElementById("mountainSelectMenu");
+    
+    mountainSelectMenu.addEventListener("change", displayMountainCard);
+})
 
 function initMountainsDropdown() {
     const mountainSelectMenu = document.getElementById("mountainSelectMenu");
@@ -12,15 +16,30 @@ function initMountainsDropdown() {
     }
 }
 
-function displayMountainCard(mountain) {
-    let mountainObject = mountain;
-    
-    const mountainName = document.getElementById("mountainName").innerText;
-    const mountainDesc = document.getElementById("mountainDesc").innerText;
-    const mountainElevation = document.getElementById("mountainElevation").innerText;
-    const mountainEffort = document.getElementById("mountainEffort").innerText;
-    const mountainCoords = document.getElementById("mountainCoords").innerText;
+function displayMountainCard() {
+    const mountainSelectMenu = document.getElementById("mountainSelectMenu");
+    const selectedMountain = mountainSelectMenu.value;
+    let mountainObject = mountainsArray[selectedMountain]
 
-    mountainName = mountainObject.name;
+    let mountainName = document.getElementById("mountainName");
+    let mountainImage = document.getElementById("mountainImage");
+    let mountainDesc = document.getElementById("mountainDesc");
+    let mountainElevation = document.getElementById("mountainElevation");
+    let mountainEffort = document.getElementById("mountainEffort");
+    let mountainCoords = document.getElementById("mountainCoords");
+
+
+
+    mountainName.innerText = mountainObject.name;
+    mountainDesc.innerText = mountainObject.desc;
+    // Storing Current text to append new
+    const effortText = mountainEffort.innerText;
+    mountainEffort.innerText = effortText + mountainObject.effort;
+    const elevationText = mountainElevation.innerText;
+    mountainElevation.innerText = elevationText + mountainObject.elevation + " ft";
+    mountainCoords.innerText = "Latitude: " + mountainObject.coords.lat + "\n" + "Longitude: " + mountainObject.coords.lng;
+
+    console.log(mountainName)
+    mountainImage.src = "/DataAssets/images/" + mountainObject.img
     mountainDesc = mountainObject.desc;
 }
